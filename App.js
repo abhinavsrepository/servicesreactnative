003631885682f7f7e521e4f132cac347a23d02ee
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-01-03 19:53:45 +0530

    login page added

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Login from './App/Screens/HomeScreens/LoginScreen/Login';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Login/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#a47feb',
+    paddingTop:20,
+    
+    
   },
 });
