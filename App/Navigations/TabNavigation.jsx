5a85f847a20da0b0545fc997e5e341f860a2ae3e
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-01-10 23:57:41 +0530

    hell

--- App/Navigations/TabNavigation.jsx
+++ App/Navigations/TabNavigation.jsx
@@ -2,6 +2,8 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import HomeScreen from '../Screens/HomeScreen/HomeScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ProfileScreen from '../Screens/ProfileScreen/ProfileScreen';
+import BookingScreen from '../Screens/BookingScreen/BookingScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -9,8 +11,8 @@ export default function TabNavigation() {
   return (
     <Tab.Navigator>
         <Tab.Screen name='home' component={HomeScreen}/>
-        <Tab.Screen name='consultation' component={Consulation}/>
-        <Tab.Screen name='profile' component={HomeScreen}/>
+        <Tab.Screen name='booking' component={BookingScreen}/>
+        <Tab.Screen name='profile' component={ProfileScreen}/>
     </Tab.Navigator>
   )
 }
