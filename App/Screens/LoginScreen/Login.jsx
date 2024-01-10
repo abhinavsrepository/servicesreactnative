523c9ab440ae362e636e719bc21dafe85dd3d93a
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-01-10 17:54:55 +0530

    color added

--- App/Screens/LoginScreen/Login.jsx
+++ App/Screens/LoginScreen/Login.jsx
@@ -1,5 +1,6 @@
 import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
+import Colors from "../../Utils/Colors";
 export default function Login() {
   return (
     <View style={{alignItems :'center'}}>
@@ -7,11 +8,23 @@ export default function Login() {
         source={require("./../../../assets/images/login.jpg")}
         style={styles.loginImage}
       />
+      <View>
+
+      </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
   loginImage: {
     width: 230,
+    height:450,
+    marginTop:70,
+    borderWidth:4,
+    borderColor:Colors.BLACK,
+    borderRadius :12
   },
+  subContainer :{
+    width:'100%',
+    backgroundColor:Colors.PRIMARY
+  }
 });
