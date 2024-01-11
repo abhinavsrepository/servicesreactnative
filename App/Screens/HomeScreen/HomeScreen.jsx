13cdafc4a360505cb2ed7771d5695aa6d7bc5be1
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-01-11 14:57:57 +0530

    hello

--- App/Screens/HomeScreen/HomeScreen.jsx
+++ App/Screens/HomeScreen/HomeScreen.jsx
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Header from './Header'
-
+import { Marquee } from '@animatereactnative/marquee'
+import { Marquee } from '@animatereactnative/marquee'
 export default function Home() {
   return (
     <View>
       <Header/>
+      <Marquee/>
     </View>
   )
 }
