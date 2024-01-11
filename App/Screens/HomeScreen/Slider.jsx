7301e04806f3dcf850546ad921d1f1fea1e9cc3c
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-01-11 23:50:25 +0530

    hello

--- App/Screens/HomeScreen/Slider.jsx
+++ App/Screens/HomeScreen/Slider.jsx
@@ -1,9 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import GlobalApi from '../../Utils/GlobalApi'
 
 export default function Slider() {
+
+    useEffect(()=>{
+        getSlider();
+
+    },[])
     const getSliders=()=>{
-        
+        GlobalApi.getSlider().then(resp=>{
+            console.log("resp",resp)
+        })
+
     }
   return (
     <View>
