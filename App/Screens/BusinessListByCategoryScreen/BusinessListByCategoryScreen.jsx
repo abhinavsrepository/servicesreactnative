9d0bc331677ce73fbcae6dfd268729950473e80d
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-01-14 23:36:44 +0530

    assets added

--- App/Screens/BusinessListByCategoryScreen/BusinessListByCategoryScreen.jsx
+++ App/Screens/BusinessListByCategoryScreen/BusinessListByCategoryScreen.jsx
@@ -1,7 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useRoute,useEffect } from '@react-navigation/native';
 
 export default function BusinessListByCategoryScreen() {
+  const param=useRoute().params;
+  useEffect(()=>{
+    console.log("Category",param.category)
+  })
   return (
     <View>
       <Text>BusinessListByCategoryScreen</Text>
--- App/Screens/HomeScreen/Categories.jsx
+++ App/Screens/HomeScreen/Categories.jsx
@@ -1,11 +1,13 @@
 import { View, Text ,StyleSheet, FlatList,Image} from 'react-native'
-import React, { useEffect ,useState} from 'react'
+import React, { useEffect ,useState,useRoute} from 'react'
 import GlobalApi from '../../Utils/GlobalApi'
 import Heading from '../../Components/Heading';
 import Colors from '../../Utils/Colors';
-
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 export default function Categories() {
     const[categories,setCategories]=useState([]);
+    const navigation =useNavigation()
     useEffect(()=>{
         getCategories();
 
@@ -24,14 +26,17 @@ export default function Categories() {
       data={categories}
       numColumns={4}
       renderItem={({item,index})=>index<=3&&(
-        <View style={styles.Container}>
+        <TouchableOpacity style={styles.Container}
+        onPress={()=>navigation.push('business-list',{
+            category:item.name
+        })}>
             <View style ={styles.iconContainer}>
                 <Image source={{uri:item?.icon?.url}}
                 style={{width:30,height:30}}
                 />
             </View>
             <Text style={{fontFamily:'outfit-medium',marginTop:5}}>{item?.name}</Text>
-        </View>
+        </TouchableOpacity>
       )}
       />
     </View>
@@ -40,13 +45,15 @@ export default function Categories() {
 const styles =StyleSheet.create({
     iconContainer:{
         backgroundColor:Colors.LIGHT_GRAY,
-        padding:17,
-        borderRadius:99
+        padding:29,
+        borderRadius:99,
+        
 
     },
     Container:{
         flex:1,
-        alignItems:'center'
+        alignItems:'center',
+        
 
     }
 })
