f5b741f086229dbbc9be9586b74d61de98c7a4b9
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-01-14 23:43:18 +0530

    assets added

--- App/Screens/BusinessListByCategoryScreen/BusinessListByCategoryScreen.jsx
+++ App/Screens/BusinessListByCategoryScreen/BusinessListByCategoryScreen.jsx
@@ -1,6 +1,8 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { useRoute,useEffect } from '@react-navigation/native';
+import { useRoute} from '@react-navigation/native';
+import React ,{useEffect} from 'react'
+import { Ionicons } from '@expo/vector-icons';
 
 export default function BusinessListByCategoryScreen() {
   const param=useRoute().params;
@@ -8,8 +10,12 @@ export default function BusinessListByCategoryScreen() {
     console.log("Category",param.category)
   })
   return (
-    <View>
-      <Text>BusinessListByCategoryScreen</Text>
+    <View style ={{padding:20,paddingTop:30}}>
+      <View>
+         <Ionicons name="arrow-back-outline" size={30} color="black" />
+    <Text style={{fontSize:25,fontFamily:'outfit-medium'}}>{param?.category}</Text>
+    </View>
+     
     </View>
   )
 }
