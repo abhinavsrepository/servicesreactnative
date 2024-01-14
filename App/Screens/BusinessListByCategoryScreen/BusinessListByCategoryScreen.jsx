159f9af0ad4d7e601b180d99873be6b09d09cd4e
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-01-14 23:50:15 +0530

    ok

--- App/Screens/BusinessListByCategoryScreen/BusinessListByCategoryScreen.jsx
+++ App/Screens/BusinessListByCategoryScreen/BusinessListByCategoryScreen.jsx
@@ -1,20 +1,22 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { useRoute} from '@react-navigation/native';
+import { useNavigation, useRoute} from '@react-navigation/native';
 import React ,{useEffect} from 'react'
 import { Ionicons } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function BusinessListByCategoryScreen() {
   const param=useRoute().params;
+  const navigation =useNavigation();
   useEffect(()=>{
     console.log("Category",param.category)
   })
   return (
     <View style ={{padding:20,paddingTop:30}}>
-      <View style={{display:'flex',flexDirection:'row',gap:10,alignItems:'center'}}>
+      <TouchableOpacity style={{display:'flex',flexDirection:'row',gap:10,alignItems:'center'}} onPress={()=>navigation.goBack()}>
          <Ionicons name="arrow-back-outline" size={30} color="black" />
     <Text style={{fontSize:25,fontFamily:'outfit-medium'}}>{param?.category}</Text>
-    </View>
+    </TouchableOpacity>
      
     </View>
   )
