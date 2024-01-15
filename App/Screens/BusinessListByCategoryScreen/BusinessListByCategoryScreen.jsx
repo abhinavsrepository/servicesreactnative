db600b8321859e3164874145367037564ea2eda2
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-01-15 11:49:57 +0530

    ok

--- App/Screens/BusinessListByCategoryScreen/BusinessListByCategoryScreen.jsx
+++ App/Screens/BusinessListByCategoryScreen/BusinessListByCategoryScreen.jsx
@@ -1,26 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useNavigation, useRoute} from '@react-navigation/native';
-import React ,{useEffect} from 'react'
-import { Ionicons } from '@expo/vector-icons';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { View, Text } from "react-native";
+import React from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import React, { useEffect } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function BusinessListByCategoryScreen() {
-  const param=useRoute().params;
-  const navigation =useNavigation();
-  useEffect(()=>{
-    console.log("Category",param.category)
-  },[])
-  const getBusinessByCategory=()=>{
-    GlobalApi.getBusinessListByCategory
-  }
+  const param = useRoute().params;
+  const navigation = useNavigation();
+  useEffect(() => {
+    console.log("Category", param.category);
+  }, []);
+  const getBusinessByCategory = () => {
+    GlobalApi.getBusinessListByCategory(param.category);
+  };
   return (
-    <View style ={{padding:20,paddingTop:30}}>
-      <TouchableOpacity style={{display:'flex',flexDirection:'row',gap:10,alignItems:'center'}} onPress={()=>navigation.goBack()}>
-         <Ionicons name="arrow-back-outline" size={30} color="black" />
-    <Text style={{fontSize:25,fontFamily:'outfit-medium'}}>{param?.category}</Text>
-    </TouchableOpacity>
-     
+    <View style={{ padding: 20, paddingTop: 30 }}>
+      <TouchableOpacity
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: 10,
+          alignItems: "center",
+        }}
+        onPress={() => navigation.goBack()}
+      >
+        <Ionicons name="arrow-back-outline" size={30} color="black" />
+        <Text style={{ fontSize: 25, fontFamily: "outfit-medium" }}>
+          {param?.category}
+        </Text>
+      </TouchableOpacity>
     </View>
-  )
+  );
 }
--- App/Screens/HomeScreen/BusinesListItemSmall.jsx
+++ App/Screens/HomeScreen/BusinesListItemSmall.jsx
@@ -1,43 +1,50 @@
-import { View, Text ,Image, StyleSheet} from 'react-native'
-import React from 'react'
-import Colors from '../../Utils/Colors'
+import { View, Text, Image, StyleSheet } from "react-native";
+import React from "react";
+import Colors from "../../Utils/Colors";
 
-export default function BusinesListItemSmall({business}) {
+export default function BusinesListItemSmall({ business }) {
   return (
     <View style={styles.container}>
-      <Image source={{uri:business?.images[0]?.url}}
-      style={styles.image}
-      />
+      <Image source={{ uri: business?.images[0]?.url }} style={styles.image} />
       <View style={styles.infoContainer}>
-        <Text style={{fontSize:17,fontFamily:'outfit-medium'}}>
-            {business?.name}
+        <Text style={{ fontSize: 17, fontFamily: "outfit-medium" }}>
+          {business?.name}
         </Text>
-        <Text style={{fontSize:13,fontFamily:'outfit',padding:3,color:Colors.PRIMARY,backgroundColor:Colors.LIGHT_BLUE,borderRadius:3,alignSelf:'flex-start',paddingHorizontal:7}}>
-            {business?.contactPerson}
+        <Text
+          style={{
+            fontSize: 13,
+            fontFamily: "outfit",
+            padding: 3,
+            color: Colors.PRIMARY,
+            backgroundColor: Colors.LIGHT_BLUE,
+            borderRadius: 3,
+            alignSelf: "flex-start",
+            paddingHorizontal: 7,
+          }}
+        >
+          {business?.contactPerson}
         </Text>
         {/* <Text>
             {business?.categor.name}
         </Text> */}
       </View>
     </View>
-  )
+  );
 }
 const styles = StyleSheet.create({
-    container:{
-        padding:10,
-        backgroundColor:Colors.WHITE,
-        borderRadius:10,
-
-
-    },
-    infoContainer:{
-      padding:7,
-      display:'flex',
-      gap:3
-    },
-    image:{
-        width:160,
-        height:100,
-        borderRadius:10,
-    }
-})
+  container: {
+    padding: 10,
+    backgroundColor: Colors.WHITE,
+    borderRadius: 10,
+  },
+  infoContainer: {
+    padding: 7,
+    display: "flex",
+    gap: 3,
+  },
+  image: {
+    width: 160,
+    height: 100,
+    borderRadius: 10,
+  },
+});
